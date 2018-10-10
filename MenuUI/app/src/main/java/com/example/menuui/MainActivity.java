0f16d66d95164a1888ca6e51a22463eb63830a9c
Matilda <mozinegbe@gmail.com>
2018-10-10 17:16:23 -0400

    Updated login page. Has some errors

--- MenuUI/app/src/main/java/com/example/menuui/MainActivity.java
+++ MenuUI/app/src/main/java/com/example/menuui/MainActivity.java
@@ -5,14 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
           setContentView(R.layout.homepage);
+
+        login_button = (Button) findViewbyId(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                sendToLogin();
+
+            }
+
+
+        });
     }
 
+
     public void sendToLogin(View view ) {
+        Intent login_intent = new Intent(this, Login.class);
+        startActivity(login_intent);
+
 
     }
 

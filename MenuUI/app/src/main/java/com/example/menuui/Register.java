01e97c65a6d06d9dfc9d4ab535e92185feed06d4
Benjamin Wolf <bwolfgns@gmail.com>
2018-10-10 18:03:10 -0400

    Added register page and assets (#1)

---
+++ MenuUI/app/src/main/java/com/example/menuui/Register.java
@@ -0,0 +1,26 @@
+package com.example.menuui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+
+    // Called when the user clicks the register button
+    public void register(View view) {
+//        Intent intent = new Intent(this, Homepage.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+    }
+}
+

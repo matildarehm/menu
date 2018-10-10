54101f6baa999f530e0ebda4fe5625aed78c1122
Ktxu18 <kristx18@gmail.com>
2018-10-10 16:37:24 -0400

    Added homepage UI

---
+++ MenuUI/app/src/main/java/com/example/menuui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.menuui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+          setContentView(R.layout.homepage);
+    }
+
+    public void sendToLogin(View view ) {
+
+    }
+
+    public void sendToRegister(View view) {
+
+    }
+}

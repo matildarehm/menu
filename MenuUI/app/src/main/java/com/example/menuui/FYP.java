4602fae68864d3d4bfe6a6f36cd2907955c825a4
Matilda <mozinegbe@gmail.com>
2018-10-14 12:10:05 -0400

    Adding FYP functionality

---
+++ MenuUI/app/src/main/java/com/example/menuui/FYP.java
@@ -0,0 +1,13 @@
+package com.example.menuui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FYP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fyp);
+    }
+}
--- MenuUI/app/src/main/java/com/example/menuui/Login.java
+++ MenuUI/app/src/main/java/com/example/menuui/Login.java
@@ -1,13 +1,34 @@
 package com.example.menuui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private TextView fyp_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        fyp_button = (TextView) findViewById(R.id.fyp);
+        fyp_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                sendToFYP();
+
+            }
+        });
+    }
+
+    public void sendToFYP() {
+        Intent fyp_intent = new Intent(this, FYP.class);
+        startActivity(fyp_intent);
+
     }
 }

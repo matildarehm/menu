cf8f9189e709f3352818feb14b9c5411e5f5418a
Matilda <mozinegbe@gmail.com>
2018-10-17 23:50:23 -0400

    rebasing with new UI

--- MenuUI/app/src/main/java/com/example/menuui/MainActivity.java
+++ MenuUI/app/src/main/java/com/example/menuui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.menuui;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button login_button;

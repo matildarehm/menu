4a8af5eb2130d7329b704a60a9a86e4fc6e870b0
Matilda <mozinegbe@gmail.com>
2018-10-17 23:58:41 -0400

    Commenting out landing and rebasing

--- MenuUI/app/src/main/java/com/example/menuui/Landing.java
+++ MenuUI/app/src/main/java/com/example/menuui/Landing.java
@@ -1,32 +1,32 @@
-package com.example.menuui;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.MotionEvent;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.SearchView;
-
-public class Landing extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
-
-        findViewById(R.id.linLayout).setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                return true;
-            }
-        });
-
-        SearchView searchView = (SearchView) findViewById(R.id.search);
-        searchView.setQueryHint("Search for food, restaurants, ...");
-        searchView.setFocusable(false);
-        searchView.setIconified(false);
-        searchView.onActionViewExpanded();
-        searchView.clearFocus();
-    }
-}
+//package com.example.menuui;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.view.MotionEvent;
+//import android.view.inputmethod.InputMethodManager;
+//import android.widget.SearchView;
+//
+//public class Landing extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_landing);
+//
+//        findViewById(R.id.linLayout).setOnTouchListener(new View.OnTouchListener() {
+//            public boolean onTouch(View v, MotionEvent event) {
+//                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+//                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+//                return true;
+//            }
+//        });
+//
+//        SearchView searchView = (SearchView) findViewById(R.id.search);
+//        searchView.setQueryHint("Search for food, restaurants, ...");
+//        searchView.setFocusable(false);
+//        searchView.setIconified(false);
+//        searchView.onActionViewExpanded();
+//        searchView.clearFocus();
+//    }
+//}

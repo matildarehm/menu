3e0d3765fda88b6ac8123722c3ef1e0755fbbe95
Ktxu18 <kristx18@gmail.com>
2018-11-05 13:09:49 -0500

    Fixed the review page

--- MenuUI/app/src/main/java/com/example/menuui/LeaveReview.java
+++ MenuUI/app/src/main/java/com/example/menuui/LeaveReview.java
@@ -1,7 +1,10 @@
 package com.example.menuui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LeaveReview extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class LeaveReview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leave_review);
+
+        // get dish name from intent -- and set the name
+        Intent intent = getIntent();
+        String dish_name = intent.getStringExtra("dish");
+        TextView dish_title = (TextView) findViewById(R.id.review_dish_name);
+        dish_title.setText(dish_name);
         
+    }
+
+    public void goBackDishPage(View view) {
+        Intent dish_page_intent = new Intent(this, DishPage.class);
+        startActivity(dish_page_intent);
     }
 }

6165252c6507f9c2a792206ee079900d0ab5ceff
JohnsonLiu25 <liuj25@rpi.edu>
2018-11-28 20:09:53 -0500

    Revert "Revert "Merge branch 'master' of github.com:liujohnson/menu""
    This reverts commit 966d851a8a13cef2e34a1d44ebc8c1db6254378d.

--- MenuUI/app/src/main/java/com/example/menuui/VerifyUser.java
+++ MenuUI/app/src/main/java/com/example/menuui/VerifyUser.java
@@ -1,28 +1,37 @@
 package com.example.menuui;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.raycoarana.codeinputview.CodeInputView;
+
+// AWS Client
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AnonymousAWSCredentials;
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.cognitoidentityprovider.AmazonCognitoIdentityProviderClient;
+
+// AWS Cognito imports
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.services.cognitoidentityprovider.AmazonCognitoIdentityProviderClient;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GenericHandler;
+import com.raycoarana.codeinputview.CodeInputView;
 
 public class VerifyUser extends AppCompatActivity {
     private Button verify_button;
-    private EditText code;
+    private CodeInputView code;
     Context context = this;
+    private AnimationDrawable cutlery_anim;
 
 
     @Override
@@ -32,15 +41,27 @@ public class VerifyUser extends AppCompatActivity {
 
         Bundle user_credentials = getIntent().getExtras();
         final String user_name = user_credentials.getString("username");
-        code = (EditText) findViewById(R.id.code_verify);
+        code = (CodeInputView) findViewById(R.id.code_verify); ;
 
+        ImageView fork_spoon = (ImageView) findViewById(R.id.verify_header);
+        fork_spoon.setImageResource(R.drawable.verify_anim);
+        cutlery_anim = (AnimationDrawable) fork_spoon.getDrawable();
 
         verify_button = (Button) findViewById(R.id.verify_pass);
         verify_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String verify_code = code.getText().toString();
+                String verify_code = code.getCode();
                 new Confirm().execute(verify_code, user_name);
+
+            }
+        });
+        TextView resend_verification = (TextView) findViewById(R.id.resend_verify);
+        resend_verification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
 
             }
         });
@@ -63,9 +84,6 @@ public class VerifyUser extends AppCompatActivity {
                 }
             };
 
-            String userPoolId = "";
-            String clientId = "";
-            String clientSecret = "";
             Region REGION = Region.getRegion(Regions.US_EAST_2);
 
             AmazonCognitoIdentityProviderClient identityProviderClient = new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials(),
@@ -85,12 +103,21 @@ public class VerifyUser extends AppCompatActivity {
             super.onPostExecute(result);
             Log.i("confirm",  "confirm result");
             go_to_landing();
-        }
 
+        }
     }
 
-    private void go_to_landing() {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        cutlery_anim.start();
+    }
+
+    public void go_to_landing() {
+        Log.i("here", "I get here.");
         Intent landing_intent = new Intent(this, Landing.class);
         startActivity(landing_intent);
     }
+
+
 }

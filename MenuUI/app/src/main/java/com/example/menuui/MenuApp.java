4a93c167b6ad14317388e0791845993892d54fb1
Ktxu18 <kristx18@gmail.com>
2018-12-05 18:34:19 -0500

    Changed octopus reviews

--- MenuUI/app/src/main/java/com/example/menuui/MenuApp.java
+++ MenuUI/app/src/main/java/com/example/menuui/MenuApp.java
@@ -22,14 +22,14 @@ public class MenuApp extends Application {
 
         octopus_reviews = new ArrayList<>();
         octopus_reviews.add(new Review("The octopus was cooked perfectly and it was delicious", 5, true, "Sally"));
-        octopus_reviews.add(new Review("This dish was fantastic! I really enjoyed it", 4, true, "Karen"));
+        octopus_reviews.add(new Review("This dish was fantastic! I really enjoyed it", 5, true, "Karen"));
         octopus_reviews.add(new Review("This is my favorite dish to get at this restaurant", 5, true, "Ronald"));
-        octopus_reviews.add(new Review("I've had better octopus at other restaurants", 3, false, "Dylan"));
+        octopus_reviews.add(new Review("I love seafood, so I enjoyed this very much", 5, false, "Dylan"));
 
         beef_stew_reviews = new ArrayList<>();
         beef_stew_reviews.add(new Review("The noodles are very chewy and the soup is very aromatic", 5, true, "Jenna"));
         beef_stew_reviews.add(new Review("The beef was cooked just right", 4, true, "Joshua"));
-        beef_stew_reviews.add(new Review("This dish was great overall", (float)4.5, true, "Anna"));
+        beef_stew_reviews.add(new Review("I've had better noodle soups elsewhere", (float)2.5, false, "Anna"));
     }
 
     List<Review> getReviews(String dish) {
